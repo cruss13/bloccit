2b1e16881d5492aff9146d63ee15cd25071ec722
Corey Russ <corey.j.russ@gmail.com>
2018-04-30 10:37:39 -0700

    Testing updated

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,11 +12,10 @@ describe('routes : static', () => {
       });
     });
     describe('GET /marco', () => {
-      it('should return status code 200 and body should contain polo', (done) => {
+      it('should return status code 200 and body should contain "polo"', () => {
         request.get('http://localhost:3000/marco', (err, res, body) => {
           expect(res.statusCode).toBe(200);
-          expect(res.body).toBe('polo');
-          done();
+          expect(body).toContain('polo');
         });
       });
     });
