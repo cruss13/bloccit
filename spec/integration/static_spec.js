5a917b878bfc3ffb544024b658b4ab4bd63996e0
Corey Russ <corey.j.russ@gmail.com>
2018-04-30 10:21:35 -0700

    Checkpoint completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,17 +5,18 @@ const base = 'http://localhost:3000/';
 describe('routes : static', () => {
 
   describe('GET /', () => {
-    it('should return status code 200', (done) => {
+    it('should return status code 200 and have "Welcome to Bloccit" in the body of the response', () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain('Welcome to Bloccit');
         done();
       });
     });
     describe('GET /marco', () => {
-      it('should return status code 200 and body should contain polo', (done) => {
+      it('should return status code 200 and body should contain "polo"', () => {
         request.get('http://localhost:3000/marco', (err, res, body) => {
           expect(res.statusCode).toBe(200);
-          expect(res.body).toBe('polo');
+          expect(body).toContain('polo');
           done();
         });
       });
