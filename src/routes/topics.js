371bbe52ecc655fb3fdecd5c6c21a27646b53605
Corey Russ <corey.j.russ@gmail.com>
2018-05-15 14:46:21 -0700

    Everything working

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const topicController = require('../controllers/topicController');
+const validation = require("./validation");
 
 router.get('/topics', topicController.index);
 
@@ -10,9 +11,9 @@ router.get("/topics/:id", topicController.show);
 
 router.get("/topics/:id/edit", topicController.edit);
 
-router.post('/topics/:id/update', topicController.update);
+router.post('/topics/:id/update', validation.validateTopics, topicController.update);
 
-router.post('/topics/create', topicController.create);
+router.post('/topics/create', validation.validateTopics, topicController.create);
 
 router.post("/topics/:id/destroy", topicController.destroy);
 
