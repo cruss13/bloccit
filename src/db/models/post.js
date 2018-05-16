45fb14344dc7c66d30de5b585441120414011c66
Corey Russ <corey.j.russ@gmail.com>
2018-05-16 10:49:49 -0700

    Tables not working

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -12,6 +12,10 @@
       topicId: {
         type: DataTypes.INTEGER,
         allowNull: false
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
       }
     }, {});
     Post.associate = function(models) {
@@ -19,7 +23,11 @@
     Post.belongsTo(models.Topic, {
        foreignKey: "topicId",
        onDelete: "CASCADE"
-     });
+    });
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE"
+    });
    };
    return Post;
  };
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -41,34 +41,47 @@ module.exports = {
     })
   },
 
-  updateTopic(id, updatedTopic, callback) {
-    return Topic.findById(id)
+  updateTopic(req, updatedTopic, callback){
+    return Topic.findById(req.params.id)
     .then((topic) => {
-      if(!topic) {
-        return callback('Topic not found');
+      if(!topic){
+        return callback("Topic not found");
       }
-      topic.update(updatedTopic, {
-        fields: Object.keys(updatedTopic)
-      })
-      .then(() => {
-        callback(null, topic);
-      })
-      .catch((err) => {
-        callback(err);
-      });
+      const authorized = new Authorizer(req.user, topic).update();
+      if(authorized) {
+        topic.update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch((err) => {
+          callback(err);
+        });
+      } else {
+        req.flash("notice", "You are not authorized to do that.");
+        callback("Forbidden");
+      }
     });
-  },
+   },
 
-  deleteTopic(id, callback){
-    return Topic.destroy({
-      where: {id}
-    })
-    .then((topic) => {
-      callback(null, topic);
-    })
-    .catch((err) => {
-      callback(err);
-    })
-  },
+  deleteTopic(req, callback){
+     return Topic.findById(req.params.id)
+     .then((topic) => {
+       const authorized = new Authorizer(req.user, topic).destroy();
+       if(authorized) {
+         topic.destroy()
+         .then((res) => {
+           callback(null, topic);
+         });
+       } else {
+         req.flash("notice", "You are not authorized to do that.")
+         callback(401);
+       }
+     })
+     .catch((err) => {
+       callback(err);
+     });
+   }
 
 }
