164bd5169a8277189c9776bd54f2662bcdf5d52e
Corey Russ <corey.j.russ@gmail.com>
2018-05-27 12:55:29 -0700

    Everything completed and working

--- src/db/models/favorite.js
+++ src/db/models/favorite.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {});
   Favorite.associate = function(models) {
-  
+
     Favorite.belongsTo(models.Post, {
       foreignKey: "postId",
       onDelete: "CASCADE"
@@ -21,6 +21,17 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       onDelete: "CASCADE"
     });
+
+    Favorite.addScope("favoritePosts", (userId) => {
+      return {
+        include: [{
+          model: models.Post
+        }],
+        where: { userId: userId },
+        order: [["createdAt", "DESC"]]
+      }
+    });
+
   };
   return Favorite;
 };
